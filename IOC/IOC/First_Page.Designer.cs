8a5d46b9782f7c52f54c8c1b416d7e9453ede3ae
Oli <Oli@DESKTOP-A7CR9RK>
2017-12-17 16:22:46 +0200

    first page added

---
+++ IOC/IOC/First_Page.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace IOC
+{
+    partial class START
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.startButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Cornsilk;
+            this.label1.Font = new System.Drawing.Font("Algerian", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(322, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RECICLAREA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.BackgroundImage = global::IOC.Properties.Resources.startButton;
+            this.startButton.Location = new System.Drawing.Point(873, 0);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(199, 179);
+            this.startButton.TabIndex = 1;
+            this.startButton.TabStop = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // START
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::IOC.Properties.Resources.firstPageBackground;
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1100, 700);
+            this.MinimumSize = new System.Drawing.Size(1100, 700);
+            this.Name = "START";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.startButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox startButton;
+    }
+}
+
---
+++ IOC/IOC/First_Page.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace IOC
+{
+    public partial class START : Form
+    {
+        public START()
+        {
+            InitializeComponent();
+
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "recs\\FirstPageRob.wav";
+            player.Play();
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
