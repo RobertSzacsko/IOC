0797603dd37a42d19291032cbc4dd8c80ddd91d6
RobertSzacsko <robiiszacsko@gmail.com>
2017-12-17 20:06:20 +0200

    Add empty sticla page

---
+++ IOC/IOC/Sticla.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace IOC
+{
+    partial class Sticla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Sticla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MaximumSize = new System.Drawing.Size(1100, 700);
+            this.MinimumSize = new System.Drawing.Size(1100, 700);
+            this.Name = "Sticla";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sticla";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
